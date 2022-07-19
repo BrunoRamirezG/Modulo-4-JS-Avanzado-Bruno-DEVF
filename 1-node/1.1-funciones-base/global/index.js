2e3619a4c04e59d15bf8d5544f6b0eaba2b26dac
BrunoRamirezG <bruno.ramirez000@outlook.com>
2022-07-19 08:23:33 -0500

    feat: agrega el gitignore

--- 1-node/1.1-funciones-base/global/index.js
+++
@@ -1,6 +0,0 @@
-console.log('Ruta donde se encuentra mi archivo: ',__dirname);
-console.log('Ruta + nombre de mi archivo: ',__filename);
-
-//exports :para exportar modulos (archivos)
-//module: referencia al modulo actual.
-//require(): Importa un archivo js como modulo. require ('index.js')
